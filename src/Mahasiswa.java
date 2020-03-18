b3441015a6774c184afdae02572b08220fc51389
Dimasshogun <dimas.gunarso@gmail.com>
2020-03-18 16:35:09 +0700

    first commit

---
+++ src/Mahasiswa.java
@@ -0,0 +1,15 @@
+
+public class Mahasiswa {
+
+    public static void main(String[] args) {
+        main mhs = new main();
+        mhs.nama = " Dimas Shofa Gunarso";
+        mhs.nim = "18090061";
+        mhs.kelas = "4B";
+        
+        System.out.println("Nama = "+mhs.nama);
+        System.out.println("Nim = "+mhs.nim);
+        System.out.println("Kelas = "+mhs.kelas);
+    }
+    
+}
